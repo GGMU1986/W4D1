12346437ffe9f663d73a0c90446c716344524863
gtsimis <gt2186@gmail.com>
2021-08-30 10:21:31 -0400

    First commit of continuation of Knights Travails project

---
+++ Knights_travails/00_tree_node.rb
@@ -0,0 +1,54 @@
+class PolyTreeNode
+    attr_reader :parent, :children, :value
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node)
+        unless parent_node.nil?
+            self.parent.children.delete(self) unless self.parent.nil?
+            @parent = parent_node
+            @parent.children << self unless @parent.children.include?(self)
+        else
+            @parent = parent_node
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        unless self.children.include?(child_node)
+            raise 'Not a child' 
+        else
+            child_node.parent = nil
+        end
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+
+        self.children.each do |child|
+            sub_answer = child.dfs(target_value)
+            return sub_answer unless sub_answer.nil?
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        queue = [self]
+
+        until queue.empty?
+            ele = queue.shift
+            return ele if ele.value == target_value
+            queue += ele.children
+        end
+        nil
+    end
+
+end
+
+
---
+++ Knights_travails/knights_travails.rb
@@ -0,0 +1,45 @@
+require_relative "00_tree_node.rb"
+
+class KnightPathFinder
+
+    def self.valid_moves(pos)
+        moves = []
+        moves.push([(pos[0] + 2), (pos[1] + 1)]) #2,1
+        moves.push([(pos[0] + 1), (pos[1] + 2)]) #1,2
+        moves.push([(pos[0] - 2), (pos[1] + 1)]) #-2,1
+        moves.push([(pos[0] - 1), (pos[1] + 2)]) #-1,2
+        moves.push([(pos[0] - 2), (pos[1] - 1)]) #-2,-1
+        moves.push([(pos[0] - 1), (pos[1] - 2)]) #-1,-2
+        moves.push([(pos[0] + 2), (pos[1] - 1)]) #2,-1
+        moves.push([(pos[0] + 1), (pos[1] - 2)]) #1,-2
+
+        rows = (0..7).to_a
+        cols = (0..7).to_a
+
+
+        moves.select do |move|
+            rows.include?(move[0]) && cols.include?(move[1]) 
+        end
+        
+    end
+
+    def initialize(pos)
+        @pos = pos
+        @root_node = PolyTreeNode.new(pos)
+        @considered_positions = [pos]
+    end
+   
+    def build_move_tree(pos)
+        queue = [pos]
+        
+        pos_children = new_move_positions(pos).map {|pos| PolyTreeNode.new(pos)}
+        queue << pos_children
+    end
+
+    def new_move_positions(pos)
+        valid_positions = KnightPathFinder.valid_moves(pos)
+        new_arr = valid_positions.reject {|pos| @considered_positions.include?(pos)}
+        @considered_positions += new_arr
+        new_arr
+    end
+end 

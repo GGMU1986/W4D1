cb72b2d705222948dfa2fe1d044fb86e250a2e02
pierre <pnavarin@gmail.com>
2021-08-30 16:03:52 -0400

    Added instance variables

--- skeleton 2/lib/tic_tac_toe_node.rb
+++ skeleton 2/lib/tic_tac_toe_node.rb
@@ -2,6 +2,10 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    p board
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +17,13 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    new_board = @board.dup
+    @board.each do |row|
+      row.each do |ele|
+        if ele.empty? && @next_mover_mark == :X
+          ele = TicTacToeNode.new(new_board, :O, :X)
+        end
+      end
+    end
   end
 end

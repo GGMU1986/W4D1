61ad3a2c1dc93e70d1fb8f5a4d0b624ddda6c13a
gtsimis <gt2186@gmail.com>
2021-08-30 17:00:22 -0400

    Add children method to TTTNode class

--- skeleton 3/lib/tic_tac_toe_node.rb
+++ skeleton 3/lib/tic_tac_toe_node.rb
@@ -1,5 +1,5 @@
 require_relative 'tic_tac_toe'
-
+require "byebug"
 class TicTacToeNode
   attr_accessor :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
@@ -17,5 +17,26 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    new_board = @board.dup
+    new_arr = []
+    # debugger
+    @board.rows.each_with_index do |row, i|
+      row.each_with_index do |ele, j|
+        if @board.empty?([i, j]) 
+          if @next_mover_mark == :x
+            node = TicTacToeNode.new(new_board, :o, [i, j])
+            pos = [i, j]
+            node.board[pos] = next_mover_mark
+            new_arr << node
+          else
+            node = TicTacToeNode.new(new_board, :x, [i, j])
+            pos = [i, j]
+            node.board[pos] = next_mover_mark
+            new_arr << node
+          end
+        end
+      end
+    end
+    new_arr
   end
 end
